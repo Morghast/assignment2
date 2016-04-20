a1f54c8e3792fca96a39a3225bbba3d47dde4916
DrTran <thegaemlostu@gmail.com>
2016-04-20 21:15:34 +1000

    sync 1.1

--- sync/sync.cpp
+++ sync/sync.cpp
@@ -2,6 +2,28 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <iostream>
-#include <unistd.h>
+#include <pthread.h>
+
+using namespace std;
+
+void * kinder_thread(void *arg)
+{
+    printf("This is kinder_thread()\n");
+    pthread_exit(NULL);
+}
+
+int main()
+{
+    pthread_t my_thread;
+    int result;
+    
+    printf("In main: creating thread\n");
+    result = pthread_create(&my_thread, NULL, &kinder_thread, NULL);
+    if(result != 0)
+    {
+        printf("Error: pthread_create() failed\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    pthread_exit(NULL);
+}
