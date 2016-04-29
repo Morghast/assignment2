e8040a9d63734e7eb250bf8878dbfeb062544096
DrTran <thegaemlostu@gmail.com>
2016-04-29 17:41:32 +1000

    sema 1.0

---
+++ sema/sema.cpp
@@ -0,0 +1,15 @@
+//Rob Davis's awesome semaphore program
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <pthread.h>
+#include <string>
+#include <mutex>
+
+using namespace std;
+
+int main()
+{
+    
+}
